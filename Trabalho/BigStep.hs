057a41281a85a9b57fa8a7b72b7bc3149179e56f
Renata <rzjunges@inf.ufpel.edu.br>
2018-06-21 20:48:21 -0300

    Comando Swap e Dupla Atribuição implementados

--- Trabalho/BigStep.hs
+++ Trabalho/BigStep.hs
@@ -21,6 +21,8 @@ data CExp =    While BExp CExp
 		| Repeat CExp BExp
 		| Do CExp BExp
 		| For AExp AExp AExp CExp
+		| Swap AExp AExp
+		| AtribDupla AExp AExp AExp AExp
 		| If BExp CExp CExp
 		| Seq CExp CExp
 		| Atrib AExp AExp
@@ -28,52 +30,73 @@ data CExp =    While BExp CExp
 	deriving(Show)                
 
 
+---------
 
 abigStep :: (AExp,Estado) -> (Int,Estado)
+
 abigStep (Var x,s) = (procuraVar s x,s)
+
 abigStep (Num n,s) = (n,s)
+
 abigStep (Som e1 e2,s)  = let	(n1,s1) = abigStep (e1, s)
 				(n2,s2) = abigStep (e2, s)
 					in (n1+n2,s)
+
 abigStep (Sub e1 e2,s)  = let 	(n1,s1) = abigStep(e1, s)
 				(n2,s2) = abigStep (e2, s)
 					in (n1-n2,s)
+
 abigStep (Mul e1 e2,s)  = let 	(n1,s1) = abigStep(e1, s)
 				(n2,s2) = abigStep (e2, s)
 					in (n1*n2,s)
 
+---------
 
 bbigStep :: (BExp,Estado) -> (Bool,Estado)
+
 bbigStep (TRUE,s)  	= (True,s)
+
 bbigStep (FALSE,s) 	= (False,s)
+
 bbigStep (Not b,s) = case bbigStep (b,s) of
 		(True,_) -> (False, s)
                 (False,_) -> (True, s)  	
+
 bbigStep (Ig e1 e2,s ) = let	(b1, s1) = abigStep(e1, s)
 				(b2, s2) = abigStep(e2, s)
 					in(b1==b2, s)
+
 bbigStep (And b1 b2,s )  = let	(n1, s1) = bbigStep(b1, s)
 				(n2, s2) = bbigStep(b2, s)
 					in(n1 && n2, s)
+
 bbigStep (Or b1 b2,s )  = let	(n1, s1) = bbigStep(b1, s)
 				(n2, s2) = bbigStep(b2, s)
 					in(n1 || n2, s)
+
 bbigStep (Leq e1 e2,s)	= let 	(n1,s1) = abigStep(e1, s)
 				(n2,s2) = abigStep (e2, s)
 					in (n1<=n2,s)
 
 
+---------
+
 cbigStep :: (CExp,Estado) -> (CExp,Estado)
+
 cbigStep (Skip,s)      	= (Skip,s)
+
 cbigStep (If b c1 c2,s) = case bbigStep(b, s) of
 			(True, _) -> cbigStep(c1,s)
 			(False,_) -> cbigStep(c2, s)
+
 cbigStep (Seq c1 c2,s)  = let	(com1, s1) = cbigStep (c1, s)
  				(com2, s2) = cbigStep(c2, s1)
 					in (com2, s2)
+
 cbigStep (Atrib (Var x) e,s) = let	(n1,s1) = abigStep(e,s)
 					(s2)	= (mudaVar s1 x n1)
 						in (cbigStep(Skip, s2))
+
 cbigStep (While b c, s) = case bbigStep(b,s) of
 			(True, _) -> cbigStep(Seq (c) (While b c), s)
 			(False, _) -> cbigStep(Skip, s)
@@ -92,8 +115,22 @@ cbigStep (For (Var x) e1 e2 c,s) = cbigStep(Seq
 					(Atrib (Var x) e1)  					
 					(If (Leq e1 e2) (Seq c (For (Var x) (Som e1 (Num 1)) e2 c)) (Skip)), s)
 
+cbigStep (Swap (Var x) (Var y), s) = let	(x_aux, s1) = abigStep(Var x, s)
+						(y_value, s2) = abigStep(Var y, s)
+						(e, new_s) = cbigStep(Atrib (Var x) (Num y_value), s)
+						(e1, final_s) = cbigStep(Atrib (Var y) (Num x_aux), new_s) 
+							in (Skip, final_s) 
+
+cbigStep (AtribDupla (Var x) (Var y) e1 e2, s) = cbigStep(Seq 
+					(Atrib (Var x) e1)
+					 (Atrib (Var y) e2), s)
+
 
 -- TESTES
+
+testeSwap :: CExp
+testeSwap = Swap (Var "x") (Var "y") 
+
 meuEstado :: Estado
 meuEstado = [("x",3), ("y",0), ("z",0)]
 
